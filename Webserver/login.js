2b3d901d5828806e98e07b1490d80aa8cc4d8997
frapatgit <frankpa1@hs-albsig.de>
2023-04-05 23:06:01 +0200

    mockup

---
+++ Webserver/login.js
@@ -0,0 +1,14 @@
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  if (username == "user" && password == "pw") {
+    //alert("success")
+    //alert("Login erfolgreich!");
+    // Navigiere zu einer anderen Seite
+    window.location.replace("./home.html");
+  } 
+  else {
+    alert("Benutzername oder Passwort falsch.");
+  }
+}

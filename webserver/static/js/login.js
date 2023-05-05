036deca0b502c31e038c60ea32a43f7bb1c6c947
frapatgit <frankpa1@hs-albsig.de>
2023-05-05 16:35:46 +0200

    fixed login issue, now enter is possible to submit

--- webserver/static/js/login.js
+++ webserver/static/js/login.js
@@ -1,17 +1,22 @@
 function login() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "/authenticate", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-          window.location.href = "/home";
-      } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
-          alert("Invalid username or password.");
-      }
-  };
-  var data = JSON.stringify({"username": username, "password": password});
-  xhr.send(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/authenticate", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            window.location.href = "/home";
+        } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
+            alert("Invalid username or password.");
+        }
+    };
+    var data = JSON.stringify({ "username": username, "password": password });
+    xhr.send(data);
 }
+var form = document.getElementById("login-form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  login();
+});

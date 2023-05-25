487b4e34671c7872537604faca04fdb39bfb0298
frapatgit <82650454+frapatgit@users.noreply.github.com>
2023-05-25 20:07:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,35 @@
-echo "# Credentials for fritzbox and VirusTotal API" >> .sh/config.ini
-echo "# adjust accordingly" >> .sh/config.ini
-echo "[credentials]" >> .sh/config
-clear
-echo "setup credentials for Virustotal API and Fritzbox:"
-read -p "Fritzbox username: " userInput
-echo "$userInput" >> .sh/config.ini
-read -p "Fritzbox passwort: " userInput
-echo "$userInput" >> .sh/config.ini
-read -p "Virustotal API Key: " userInput
-echo "$userInput" >> .sh/config.ini
-echo "[#] creating certificates for the webserver..."
+cd sh
+if [ ! -e "config.ini" ]; then
+        echo "# Credentials for fritzbox and VirusTotal API" >> .sh/config.ini
+        echo "# adjust accordingly" >> .sh/config.ini
+        echo "[credentials]" >> .sh/config
+        clear
+        echo "setup credentials for Virustotal API and Fritzbox:"
+        read -p "Fritzbox username: " userInput
+        echo "$userInput" >> sh/config.ini
+        read -p "Fritzbox passwort: " userInput
+        echo "$userInput" >> sh/config.ini
+        read -p "Virustotal API Key: " userInput
+        echo "$userInput" >> sh/config.ini
+fi
+cd ..
 cd webserver
-mkdir cert
-cd cert
-openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout key.pem -out cert.pem -subj "/CN=TinyNetworkSniffer" -days 365
-echo "[#] certs created"
+if [ ! -d "cert" ]; then
+        echo "[#] creating certificates for the webserver..."
+        mkdir cert
+        cd cert
+        openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout key.pem -out cert.pem -subj "/CN=TinyNetworkSniffer" -days 365
+        echo "[#] certs created"
+fi
 cd ..
 echo "[#] building docker"
-cd ..
-bash ./docker-build.sh
+bash docker-build.sh
 echo "[#] built docker "
 echo "[#] starting docker-container"
-bash ./docker-run.sh
+bash docker-run.sh
 echo "[#] webserver is ready"
 echo "[#] starting monitoring"
-bash ./sh/main.sh
+cd ./sh
+bash nohup main.sh &
 echo "[#] monitoring started"
 echo "[#] setup completed"
